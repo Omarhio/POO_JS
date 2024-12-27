e30d9e5e0ecd4190b32a5bf1e1829afffb2fc2b1
Omarhio <big-dz@hotmail.fr>
2024-12-27 15:56:27 +0100

    feat: Application des getters et setters

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 class Voiture {
 
     constructor(marque, modele, annee, couleur) {
-        this.marque = marque;
-        this.modele = modele;
-        this.annee = annee;
-        this.couleur = couleur;
+        this._marque = marque;
+        this._modele = modele;
+        this._annee = annee;
+        this._couleur = couleur;
     }
-    afficherDetails() {
-        console.log(this.marque)
-        console.log(this.modele)
-        console.log(this.annee)
-        console.log(this.couleur)
+
+    get couleurs() {
+        return this._couleur;
     }
+
+    set couleurs(nouvelleCouleur) {
+        this._couleur = nouvelleCouleur;
+    }
+
 }
 
-const voiture = new Voiture("Toyota", "Yaris", "2024", "Blanche");
+const voiture = new Voiture("Toyota", "Corolla", 2024, "Vert");
 
-voiture.afficherDetails(voiture);
+console.log(voiture.couleurs);
